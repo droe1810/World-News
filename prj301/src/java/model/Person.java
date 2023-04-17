059d22f11dfccf4cc067bf5dbc20ebd4ca5ca210
droe1810 <havietdanh1810@gmail.com>
2023-04-17 21:19:11 +0700

    create Person.java
    create Person.java

---
+++ prj301/src/java/model/Person.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.Date;
+
+
+public class Person {
+    private int id;
+    private String name;
+    private Date dob;
+    private String address;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, Date dob, String address) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + ", dob=" + dob + ", address=" + address + '}';
+    }
+
+    
+    
+}
