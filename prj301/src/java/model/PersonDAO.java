e636e502e0f24c03296566a325bd775b68083f7f
droe1810 <havietdanh1810@gmail.com>
2023-04-17 23:55:39 +0700

    Update PersonDAO.java
    add method getPersonByNewID()

--- prj301/src/java/model/PersonDAO.java
+++ prj301/src/java/model/PersonDAO.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+public class PersonDAO extends MyDAO {
 
-public class PersonDAO extends MyDAO{
-       public List<Person> getAllPerson() {
+    public List<Person> getAllPerson() {
         List<Person> list = new ArrayList<>();
         xSql = "select * from person";
         try {
@@ -29,5 +29,32 @@ public class PersonDAO extends MyDAO{
             e.printStackTrace();
         }
         return list;
+    }
+
+    public Person getPersonByNewID(int id) {
+        xSql = "select pa.id,pa.name,pa.dob,pa.address from (select * from person p inner join Account a on p.id = a.person_id) pa "
+                + "inner join new n on pa.person_id = n.writerID where n.id = ?";
+        int xId;
+        String xName;
+        Date xDob;
+        String xAddress;
+        Person x = null;
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                xId = rs.getInt("id");
+                xName = rs.getString("name");
+                xDob = rs.getDate("dob");
+                xAddress = rs.getString("address");
+                x = new Person(id, xName, xDob, xAddress);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return x;
+
     }
 }
