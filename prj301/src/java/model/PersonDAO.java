c258a6421b799c5212102e8ace03509f2e85b8ca
droe1810 <havietdanh1810@gmail.com>
2023-04-17 23:45:51 +0700

    Update PersonDAO.java
    add method getAllPerson()

--- prj301/src/java/model/PersonDAO.java
+++ prj301/src/java/model/PersonDAO.java
@@ -1,6 +1,33 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-public class PersonDAO {
-    
+
+public class PersonDAO extends MyDAO{
+       public List<Person> getAllPerson() {
+        List<Person> list = new ArrayList<>();
+        xSql = "select * from person";
+        try {
+            int xId;
+            String xName;
+            Date xDob;
+            String xAddress;
+            Person x;
+            ps = con.prepareStatement(xSql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                xId = rs.getInt("id");
+                xName = rs.getString("name");
+                xDob = rs.getDate("dob");
+                xAddress = rs.getString("address");
+                x = new Person(xId, xName, xDob, xName);
+                list.add(x);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
