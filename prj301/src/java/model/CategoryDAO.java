65709f46beff578fbf45549209db8dd5ef601ec9
droe1810 <havietdanh1810@gmail.com>
2023-04-17 23:36:11 +0700

    Update CategoryDAO.java
    add method getAllCategory() and getCategoryByID()

--- prj301/src/java/model/CategoryDAO.java
+++ prj301/src/java/model/CategoryDAO.java
@@ -1,5 +1,51 @@
 package model;
 
-public class CategoryDAO {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryDAO extends MyDAO {
+
+    public List<Category> getAllCategory() {
+        List<Category> list = new ArrayList<>();
+        xSql = "select * from category";
+        try {
+            String xCname;
+            int xCid;
+            Category x;
+            ps = con.prepareStatement(xSql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                xCid = rs.getInt("cid");
+                xCname = rs.getString("cname");
+                x = new Category(xCid, xCname);
+                list.add(x);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public Category getCategoryByID(String id) {
+        xSql = "select * from category where cid = ?";
+        String xCname;
+        int xCid;
+        Category x = null;
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                xCid = rs.getInt("cid");
+                xCname = rs.getString("cname");
+                x = new Category(xCid, xCname);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return x;
+
+    }
 }
