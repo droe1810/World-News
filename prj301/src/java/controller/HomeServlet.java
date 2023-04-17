48a8c3b0abf82367284625a116fc2fa5a592487d
droe1810 <havietdanh1810@gmail.com>
2023-04-18 00:12:47 +0700

    create HomeServlet
    create HomeServlet

---
+++ prj301/src/java/controller/HomeServlet.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import model.*;
+import java.util.*;
+
+public class HomeServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String sId = request.getParameter("cid");
+        if(sId == null){
+            sId = "1";
+        }
+        int id = Integer.parseInt(sId);
+        
+        NewDAO nd = new NewDAO();
+        CategoryDAO cd = new CategoryDAO();
+        
+        List<New> listN = nd.getNewByCID(id);
+        List<Category> listC = cd.getAllCategory();
+        Category c = cd.getCategoryByID(sId);
+        
+        request.setAttribute("listN", listN);
+        request.setAttribute("listC", listC);
+        request.setAttribute("c", c);
+        
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+
+    }
+
+}
